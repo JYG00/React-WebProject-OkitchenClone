a4915f3a44a526d56ccff405629c31124e9a9041
JYG00 <usbdsu@naver.com>
2022-06-27 11:05:49 +0900

    20220627 1105

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,29 @@ import React, { useEffect, useState } from 'react';
 import style from './main.module.css';
 import Footer from './footer';
 import { useRef } from 'react';
+// 메인 슬라이드(carousel) 이미지
+import main01 from './img/main01.jpg';
+import main02 from './img/main02.jpg';
+import main03 from './img/main03.jpg';
+import main04 from './img/main04.jpg';
+import main05 from './img/main05.jpg';
+import main06 from './img/main06.jpg';
+import main07 from './img/main07.jpg';
 
 function Main() {
   const mainRef = useRef();
 
-  const [xPosition, setXPosition] = useState(-2381);
+  const [xPosition, setXPosition] = useState(-2000);
 
   // 왼쪽 화살표 버튼을 누를경우
   const onClickLeft = () => {
     mainRef.current.style.transition = 'all 0.3s ease-in-out';
-    set(xPosition + 2381);
+    set(xPosition + 2000);
   };
   // 오른쪽 화살표 버튼을 누를경우
   const onClickRight = () => {
     mainRef.current.style.transition = 'all 0.3s ease-in-out';
-    set(xPosition - 2381);
+    set(xPosition - 2000);
   };
 
   const set = (param) => {
@@ -24,63 +32,49 @@ function Main() {
   };
 
   useEffect(() => {
+    // xPosition에 따라서 슬라이드 위치 변경
     mainRef.current.style.transition = 'all 0.3s ease-in-out';
     mainRef.current.style.left = `${xPosition}px`;
-    // 마지막 페이지에 도달하면 첫 페이지로 이동
-    if (xPosition === -19048) {
+    // 마지막 페이지에서 첫 페이지로 부드럽게 이동
+    if (xPosition === -16000) {
       mainRef.current.style.transition = 'none';
       mainRef.current.style.left = 0;
-      set(-2381);
+      set(-2000);
     }
-    // 첫 페이지에 도달하면 마지막 페이지로 이동
+    // 첫 페이지에서 마지막 페이지로 부드럽게 이동
     if (xPosition === 0) {
       mainRef.current.style.transition = 'none';
-      mainRef.current.style.left = '-19048px';
-      set(-16667);
+      mainRef.current.style.left = '-16000px';
+      set(-14000);
     }
   }, [xPosition]);
 
   return (
     <div className={style.main}>
-      {/* 메인이미지 */}
+      {/* 메인 슬라이드 */}
       <div className={style.mainImg}>
         <div className={style.mainSlideBtn_container}>
-          {/* 왼쪽 화살표 버튼 */}
-          <div className={style.mainSlideBtn} id={style.mainSlideLeft} onClick={onClickLeft}></div>
-          {/* 오른쪽 화살표 버튼 */}
-          <div className={style.mainSlideBtn} id={style.mainSlideRight} onClick={onClickRight}></div>
+          <div>
+            {/* 왼쪽 화살표 버튼 */}
+            <div className={style.mainSlideBtn} id={style.mainSlideLeft} onClick={onClickLeft}></div>
+            {/* 오른쪽 화살표 버튼 */}
+            <div className={style.mainSlideBtn} id={style.mainSlideRight} onClick={onClickRight}></div>
+          </div>
         </div>
         <div className={style.mainSlideListBtn_container}>
           <div></div>
         </div>
+        {/* 메인 슬라이드 이미지*/}
         <div className={style.mainImg_in} ref={mainRef}>
-          <div className={style.mainSlide}>
-            <img src={require('./img/main07.jpg')} alt="메인 슬라이드" />
-          </div>
-          <div className={style.mainSlide}>
-            <img src={require('./img/main01.jpg')} alt="메인 슬라이드" />
-          </div>
-          <div className={style.mainSlide}>
-            <img src={require('./img/main02.jpg')} alt="메인 슬라이드" />
-          </div>
-          <div className={style.mainSlide}>
-            <img src={require('./img/main03.jpg')} alt="메인 슬라이드" />
-          </div>
-          <div className={style.mainSlide}>
-            <img src={require('./img/main04.jpg')} alt="메인 슬라이드" />
-          </div>
-          <div className={style.mainSlide}>
-            <img src={require('./img/main05.jpg')} alt="메인 슬라이드" />
-          </div>
-          <div className={style.mainSlide}>
-            <img src={require('./img/main06.jpg')} alt="메인 슬라이드" />
-          </div>
-          <div className={style.mainSlide}>
-            <img src={require('./img/main07.jpg')} alt="메인 슬라이드" />
-          </div>
-          <div className={style.mainSlide}>
-            <img src={require('./img/main01.jpg')} alt="메인 슬라이드" />
-          </div>
+          <div className={style.mainSlide} style={{ background: `url(${main07}) no-repeat center`, backgroundSize: 'cover' }}></div>
+          <div className={style.mainSlide} style={{ background: `url(${main01}) no-repeat center`, backgroundSize: 'cover' }}></div>
+          <div className={style.mainSlide} style={{ background: `url(${main02}) no-repeat center`, backgroundSize: 'cover' }}></div>
+          <div className={style.mainSlide} style={{ background: `url(${main03}) no-repeat center`, backgroundSize: 'cover' }}></div>
+          <div className={style.mainSlide} style={{ background: `url(${main04}) no-repeat center`, backgroundSize: 'cover' }}></div>
+          <div className={style.mainSlide} style={{ background: `url(${main05}) no-repeat center`, backgroundSize: 'cover' }}></div>
+          <div className={style.mainSlide} style={{ background: `url(${main06}) no-repeat center`, backgroundSize: 'cover' }}></div>
+          <div className={style.mainSlide} style={{ background: `url(${main07}) no-repeat center`, backgroundSize: 'cover' }}></div>
+          <div className={style.mainSlide} style={{ background: `url(${main01}) no-repeat center`, backgroundSize: 'cover' }}></div>
         </div>
       </div>
       {/* 인기 메뉴 */}
